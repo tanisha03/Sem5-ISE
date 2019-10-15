6f84b6b8cf646940c5ec588dfb7a9dd2e3739c04
Tanisha <tanisha031199@gmail.com>
2019-10-16 00:43:26 +0530

    updated

--- CIE-1/SLL/4/main.py
+++ CIE-1/SLL/4/main.py
@@ -3,19 +3,20 @@ import sys,os
 dict = {}
 wordLen = []
 
-if(len(sys.argv) != 2):
+if(len(sys.argv) != 3):
 	print ("Invalid Arguments")
+	print(sys.argv)
 	sys.exit()
 
-if(not(os.path.exists(sys.argv[0]))):
+if(not(os.path.exists(sys.argv[1]))):
 	print ("Invalid File Path")
 	sys.exit()
 
-if(sys.argv[1].split('.')[-1] != "txt"):
+if(sys.argv[2].split('.')[-1] != "txt"):
 	print ("Invalid File Format. Only TXT files allowed")
 	sys.exit()
 
-with open(sys.argv[1]) as file:
+with open(sys.argv[2]) as file:
 	for line in file:
 		for word in line.split():
 			dict[word] = dict.get(word,0) + 1

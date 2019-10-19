b781dfbd799c5a240948be54ebdfd4facd0b0226
Tanisha <tanisha031199@gmail.com>
2019-10-19 19:43:13 +0530

    added 5b

--- CN-Lab/5b/5b.tcl
+++ CN-Lab/5b/5b.tcl
@@ -17,7 +17,7 @@ proc finish {}   {
     close $traceFile
     close $namFile
     #calculate packet drops
-    exec awk -f stats.awk 5.tr &
+    exec gawk -f stats.awk 5.tr &
     exec nam 5.nam &
     exit 0
 }
@@ -42,37 +42,54 @@ $n(4) color blue
 #Create links between the nodes
 $ns duplex-link $n(0) $n(2) 1Mb 10ms DropTail
 $ns duplex-link $n(1) $n(2) 1Mb 10ms DropTail
-$ns duplex-link $n(2) $n(3) 1Mb 10ms DropTail
-$ns duplex-link $n(3) $n(2) 1Mb 10ms DropTail
+$ns simplex-link $n(2) $n(3) 0.3Mb 100ms DropTail
+$ns simplex-link $n(3) $n(2) 0.3Mb 100ms DropTail
 
-# Set LAN
+#Create a LAN setup
 set lan [$ns newLan "$n(3) $n(4) $n(5) $n(6)" 0.5Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
 
-# position nodes
+#position the nodes for NAM
 $ns duplex-link-op $n(0) $n(2) orient right-down
 $ns duplex-link-op $n(1) $n(2) orient right-up
 $ns simplex-link-op $n(2) $n(3) orient right
 $ns simplex-link-op $n(3) $n(2) orient left
 
-# create TCP agent
+#Create a TCP Agent
 set TCPAgent [new Agent/TCP/Newreno]
 $ns attach-agent $n(0) $TCPAgent
 $TCPAgent set fid_ 1
 $TCPAgent set packetSize_ 552
 
-# create TCP Sink
+#Create a TCP Sink
 set TCPSink [new Agent/TCPSink/DelAck]
 $ns attach-agent $n(4) $TCPSink
 $ns connect $TCPAgent $TCPSink
 
-# Attach FTP Application to the TCP Agent
+#Attach a FTP Application to the TCP Agent
 set ftp [new Application/FTP]
 $ftp attach-agent $TCPAgent
 
-# Create UDP Agent
+#Create a UDP Agent
 set UDPAgent [new Agent/UDP]
 $ns attach-agent $n(1) $UDPAgent
 $UDPAgent set fid_ 2
 set nullSink [new Agent/Null]
 $ns attach-agent $n(6) $nullSink
+$ns connect $UDPAgent $nullSink
 
+#Attach a CBR Application to the UDP Agent
+set  cbr [new Application/Traffic/CBR]
+$cbr attach-agent $UDPAgent
+$cbr set type_ CBR
+$cbr set packetSize 1000
+$cbr set rate_ 0.05Mb
+$cbr set random_ false
+
+$ns at 0.0 "$n(0) label TCP_Traffic"
+$ns at 0.0 "$n(1) label UDP_Traffic"
+$ns at 0.3 "$cbr start"
+$ns at 0.8 "$ftp start"
+$ns at 7.0 "$ftp stop"
+$ns at 7.5 "$cbr stop"
+$ns at 8.0 "finish"
+$ns run

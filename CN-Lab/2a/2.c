d8c80bddec0ffb039b1264b740fb05b1eb3b6c05
Tanisha <tanisha031199@gmail.com>
2019-11-21 22:18:56 +0530

    added

---
+++ CN-Lab/2a/2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MIN(x,y) (x>y)?y:x
+
+int main()
+{
+int orate=2,drop=0,cap=5,x,count=0,inp[10]={0},nsec,ch,i=0;
+for(i=0;i<5;i++)
+	inp[i]=rand()%10;
+nsec=i;
+printf("\n second       recieved        sent      dropped       remained \n");
+for(i=0;count || i<nsec;i++)
+{
+ printf("%d",i+1);
+ printf(" \t %d\t ",inp[i]);
+ printf(" \t %d\t ",MIN((inp[i]+count),orate));
+ if((x=inp[i]+count-orate)>0)
+ {
+  if(x>cap)
+  {
+   count=cap;
+   drop=x-cap;
+  }
+  else
+  {
+   count=x;
+   drop=0;
+  }
+ }
+ else
+ {
+  drop=0;
+  count=0;
+  }
+ printf(" \t %d \t %d \n",drop,count);
+ }   // end of for loop
+ return 0;
+}

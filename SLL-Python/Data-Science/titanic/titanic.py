c1f7e2104641bacec85fd7ca18d866bc673d8bd6
Tanisha <tanisha031199@gmail.com>
2019-11-26 01:20:58 +0530

    added

--- SLL-Python/Data-Science/titanic/titanic.py
+++ SLL-Python/Data-Science/titanic/titanic.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-titanic_df = pd.read_csv("train.csv")
+titanic_df = pd.read_csv("Titanic.csv")
 
 # preview the data
 print("======Data Headers=======")
@@ -14,3 +14,5 @@ print("=====Data Decription=====")
 # column description
 titanic_df.info()
 titanic_df.describe()
+
+titanic_df = titanic_df.drop(["PassengerId", "Name", "Ticket", "Parch"], axis=1)

59741ae9683a661ba8bd0dec71cfda2b02a7e9b4
Tanisha <tanisha031199@gmail.com>
2019-11-26 23:56:28 +0530

    Added

--- SLL-Python/Data-Science/Black-Friday/blackfriday.py
+++ SLL-Python/Data-Science/Black-Friday/blackfriday.py
@@ -1,10 +1,9 @@
 import pandas as pd
-from pandas import Series, DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data=pd.read_csv("BlackFriday.csv")
+data = pd.read_csv("BlackFriday.csv")
 
 print("======Data Headers=======")
 print(data.head())
@@ -13,25 +12,27 @@ print("=====Data Decription=====")
 print(data.info())
 print(data.describe())
 
-data=data.drop(['User_ID', 'Product_ID', 'Stay_In_Current_City_Years'], axis=1)
+data = data.drop(["User_ID", "Product_ID", "Stay_In_Current_City_Years"], axis=1)
 print(data.head())
 
 data["City_Category"] = data["City_Category"].fillna("B")
 print(data["City_Category"])
 
-data["City_Category"] = data['City_Category'].map({
-    "A": "Metro Cities",
-    "B": "Small Towns",
-    "C": "Villages"
-})
+data["City_Category"] = data["City_Category"].map(
+    {"A": "Metro Cities", "B": "Small Towns", "C": "Villages"}
+)
 
 
-data["Marital_Status"] = data['Marital_Status'].map({
-    1: "Married",
-    0: "Un-married"
-})
+data["Marital_Status"] = data["Marital_Status"].map({1: "Married", 0: "Un-married"})
 
-data.rename(columns={"Product_Category_1":"Baseball Caps","Product_Category_2":"Wine tumblers","Product_Category_3":"Pet Raincoats"},inplace=True)
+data.rename(
+    columns={
+        "Product_Category_1": "Baseball Caps",
+        "Product_Category_2": "Wine tumblers",
+        "Product_Category_3": "Pet Raincoats",
+    },
+    inplace=True,
+)
 
 data["Wine tumblers"] = data["Wine tumblers"].fillna("5")
 data["Pet Raincoats"] = data["Pet Raincoats"].fillna("10")

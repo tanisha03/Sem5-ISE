93f382223306a672c092b4aa73e264cb6d713c0d
Tanisha <tanisha031199@gmail.com>
2019-12-03 22:14:21 +0530

    added

--- SLL-Python/CIE-2/titanic/titanic.py
+++ SLL-Python/CIE-2/titanic/titanic.py
@@ -32,9 +32,9 @@ def get_person(passenger):
 
 titanic_df["Person"] = titanic_df[["Age", "Sex"]].apply(get_person, axis=1)
 
-person_dummies_titanic = pd.get_dummies(titanic_df["Person"])
-person_dummies_titanic.columns = ["Child", "Female", "Male"]
-person_dummies_titanic.drop(["Male"], axis=1, inplace=True)
+# person_dummies_titanic = pd.get_dummies(titanic_df["Person"])
+# person_dummies_titanic.columns = ["Child", "Female", "Male"]
+# person_dummies_titanic.drop(["Male"], axis=1, inplace=True)
 
 
 titanic_df["Survived"] = titanic_df["Survived"].map({0: "Died", 1: "Survived"})
@@ -67,22 +67,22 @@ plt.show()
 # fig, (axis1, axis2) = plt.subplots(1, 2, figsize=(10, 5))
 # g = sns.factorplot("Pclass", data=titanic_df, kind="count", ax=axis1)
 
-print(person_dummies_titanic)
+# print(person_dummies_titanic)
 
-person_perc = (
-    titanic_df[["Person", "Survived"]].groupby(["Person"], as_index=False).mean()
-)
-print(person_perc)
+# person_perc = (
+#     titanic_df[["Person", "Survived"]].groupby(["Person"], as_index=False).mean()
+# )
+# print(person_perc)
 
-sns.barplot(
-    x="Person", y="Survived", data=person_perc, order=["male", "female", "child"],
-)
-plt.show()
-"""No of passedngers in each group """
+# sns.barplot(
+#     x="Person", y="Survived", data=person_perc, order=["male", "female", "child"],
+# )
+# plt.show()
+# """No of passedngers in each group """
 
 
-facet = sns.FacetGrid(titanic_df, hue="Survived", aspect=4)
-facet.map(sns.kdeplot, "Age", shade=True)
-facet.set(xlim=(0, titanic_df["Age"].max()))
-facet.add_legend()
-plt.show()
+# facet = sns.FacetGrid(titanic_df, hue="Survived", aspect=4)
+# facet.map(sns.kdeplot, "Age", shade=True)
+# facet.set(xlim=(0, titanic_df["Age"].max()))
+# facet.add_legend()
+# plt.show()

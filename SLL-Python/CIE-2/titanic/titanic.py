85f655625ad53ae72cbd259cbc53d789d864b040
Tanisha <tanisha031199@gmail.com>
2019-12-03 22:15:30 +0530

    added

--- SLL-Python/CIE-2/titanic/titanic.py
+++ SLL-Python/CIE-2/titanic/titanic.py
@@ -62,27 +62,3 @@ ax.set(
 )
 plt.show()
 
-# titanic_df = titanic_df.join(person_dummies_titanic)
-# print(titanic_df)
-# fig, (axis1, axis2) = plt.subplots(1, 2, figsize=(10, 5))
-# g = sns.factorplot("Pclass", data=titanic_df, kind="count", ax=axis1)
-
-# print(person_dummies_titanic)
-
-# person_perc = (
-#     titanic_df[["Person", "Survived"]].groupby(["Person"], as_index=False).mean()
-# )
-# print(person_perc)
-
-# sns.barplot(
-#     x="Person", y="Survived", data=person_perc, order=["male", "female", "child"],
-# )
-# plt.show()
-# """No of passedngers in each group """
-
-
-# facet = sns.FacetGrid(titanic_df, hue="Survived", aspect=4)
-# facet.map(sns.kdeplot, "Age", shade=True)
-# facet.set(xlim=(0, titanic_df["Age"].max()))
-# facet.add_legend()
-# plt.show()

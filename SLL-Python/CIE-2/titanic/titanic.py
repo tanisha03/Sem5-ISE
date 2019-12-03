eb6503c6f9fffb5f52a87084e66d30d94856c4f4
Tanisha <tanisha031199@gmail.com>
2019-12-03 22:12:53 +0530

    added

--- SLL-Python/CIE-2/titanic/titanic.py
+++ SLL-Python/CIE-2/titanic/titanic.py
@@ -32,13 +32,35 @@ def get_person(passenger):
 
 titanic_df["Person"] = titanic_df[["Age", "Sex"]].apply(get_person, axis=1)
 
-# print(titanic_df["Person"])
-
-
 person_dummies_titanic = pd.get_dummies(titanic_df["Person"])
 person_dummies_titanic.columns = ["Child", "Female", "Male"]
 person_dummies_titanic.drop(["Male"], axis=1, inplace=True)
 
+
+titanic_df["Survived"] = titanic_df["Survived"].map({0: "Died", 1: "Survived"})
+print(titanic_df.Survived)
+ax = sns.countplot(x="Pclass", hue="Survived", palette="Set1", data=titanic_df)
+ax.set(
+    title="Passenger status (Survived/Died) against Passenger Class",
+    xlabel="Passenger Class",
+    ylabel="Total",
+)
+plt.show()
+
+ax = sns.countplot(x="Sex", hue="Survived", palette="Set2", data=titanic_df)
+ax.set(title="Total Survivors According to Sex", xlabel="Sex", ylabel="Total")
+plt.show()
+
+interval = (0, 18, 35, 60, 120)
+categories = ["Children", "Teens", "Adult", "Old"]
+titanic_df["Age_cats"] = pd.cut(titanic_df.Age, interval, labels=categories)
+ax = sns.countplot(x="Age_cats", data=titanic_df, hue="Survived", palette="Set3")
+ax.set(
+    xlabel="Age Categorical",
+    ylabel="Total",
+    title="Age Categorical Survival Distribution",
+)
+plt.show()
 
 # titanic_df = titanic_df.join(person_dummies_titanic)
 # print(titanic_df)

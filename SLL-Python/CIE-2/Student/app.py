b69909574ae932cabd92e85030879ed0d9d53945
Tanisha <tanisha031199@gmail.com>
2019-12-03 18:28:11 +0530

    added

---
+++ SLL-Python/CIE-2/Student/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, redirect, render_template, request, url_for, request
+import time  # time and date related functions
+import re  # regular expressions
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+
+    if request.method == "POST":
+
+        # Check if form fields are empty
+        if request.form["usn"] == "" or request.form["dob"] == "" or request.form["marks1"] == "" or request.form["marks2"] == "" or request.form["marks3"] == "":
+            msg = "***All form fields are required***"
+            return render_template("index.html", msg=msg)
+
+        # Check if date entered in dd/mm/yyyy format and is not an invalid date Eg. 31/11/2016
+        # Use strptime() function which raises an exception if date is invalid
+        try:
+            time.strptime(request.form["dob"], "%d/%m/%Y")
+        except ValueError:
+            msg = "***Date is invalid***"
+            return render_template("index.html", msg=msg)
+
+        # Regex for USN
+        usn_pattern = "^[1][A-Z][A-Z][0-9][0-9][A-Z][A-Z][0-9][0-9][0-9]$"
+
+        # Check if entered USN matches Regex
+        if not re.match(usn_pattern, request.form["usn"]):
+            msg = "***USN format invalid***"
+            return render_template("index.html", msg=msg)
+
+        # If form fields are valid return success HTML page
+        b=(int(request.form["marks1"])+int(request.form["marks2"])+int(request.form["marks3"]))/3
+        return render_template("success.html", marks=b)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+

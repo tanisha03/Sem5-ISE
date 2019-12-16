17d6d30cf28354f454027c2410ab8e31294e8c96
Tanisha <tanisha031199@gmail.com>
2019-12-16 23:13:14 +0530

    added doc

--- SLL/SEE/4a.py
+++
@@ -1,9 +0,0 @@
-class Rectangle:
-    def __init__(self,a,b):
-        self.len=a
-        self.breadth=b
-    def area(self):
-        return self.len*self.breadth
-
-rec=Rectangle(4,5)
-print(rec.area())

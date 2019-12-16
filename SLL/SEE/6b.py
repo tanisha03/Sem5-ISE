4acd49ae002766cf506ddb14f9c34d69b0e712e7
Tanisha <tanisha031199@gmail.com>
2019-12-16 22:06:13 +0530

    added SEE

---
+++ SLL/SEE/6b.py
@@ -0,0 +1,21 @@
+class Sentence_rev:
+    s=""
+    rev_s=""
+    vowels=['a','e','i','o','u']
+    def __init__(self,sentence):
+        self.s=sentence
+        self.rev_s=' '.join(reversed(self.s.split()))
+    def ret_rev(self):
+        return self.rev_s
+    def count_vow(self):
+        return sum(s in self.vowels for s in self.s.lower())
+
+l1=[]
+
+for i in range(3):
+    reverser=Sentence_rev(input("enter a string\n"))
+    l1.append(reverser)
+
+for i in sorted(l1,key=lambda i:i.count_vow(),reverse=True):
+    print(i.ret_rev())
+

08de810a92bffc41a77343ad321aeac377e68ece
Tanisha <tanisha031199@gmail.com>
2019-12-26 13:09:27 +0530

    added

--- DBMS-Lab/3a.sql
+++ DBMS-Lab/3a.sql
@@ -70,3 +70,16 @@ SELECT COUNT(BID), SID
 FROM RESERVES
 GROUP BY SID;
 
+
+-- MONGODB
+
+Create the collection:db.createCollection("BOATRES")
+
+db.BOATRES.insert({"BID":9988,"BNAME":'ABC',"COLOUR":'Black',"SNAME":'John',"SID":1234,"DAY":'2017-12-25'})
+ db.BOATRES.insert({"BID":8877,"BNAME":'DEF',"COLOUR":'Black',"SNAME":'Smith',"SID":4567,"DAY":'2017-11-24'})
+db.BOATRES.insert({"BID":7766,"BNAME":'GHI',"COLOUR":'Blue',"SNAME":'Sucre',"SID":1122,"DAY":'2017-10-24'})
+db.BOATRES.insert({"BID":6655,"BNAME":'PQR',"COLOUR":'white',"SNAME":'Sucre',"SID":1122,"DAY":'2017-10-29'})
+
+db.BOATRES.find({"SNAME":'Sucre'}).count()
+db.BOATRES.find({"COLOUR":'Black'}).pretty()
+
